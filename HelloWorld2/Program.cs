d6840c45e219bceee07b6d13ee57def7e6524593
Kajetan <kajetan.duszynski@jit.team>
2023-06-21 15:47:14 +0200

    merge-problem-test1

--- HelloWorld2/Program.cs
+++ HelloWorld2/Program.cs
@@ -2,3 +2,10 @@
 Console.WriteLine("Cześć Kajetan");
 
 Console.WriteLine("Hello Someone");
+
+public void TestMethod()
+{
+    Console.WriteLine("Cześć Kajetan");
+
+    Console.WriteLine("Hello Someone");
+}
